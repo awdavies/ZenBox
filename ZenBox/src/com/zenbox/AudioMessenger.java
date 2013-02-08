add9e5da40645e0e4d2ffc4641fcd04789c41f97
Chris Bruce <aprofessionalemailaddress@gmail.com>
2013-02-07 17:58:22 -0800

    Add basic sound support

---
+++ ZenBox/src/com/zenbox/AudioMessenger.java
@@ -0,0 +1,140 @@
+package com.zenbox;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.puredata.android.service.PdService;
+import org.puredata.core.PdBase;
+import org.puredata.core.utils.IoUtils;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.res.Resources;
+import android.os.IBinder;
+import android.util.Log;
+
+/**
+ * Singleton class to set up a connection to the PD service
+ * and facilitate message passing.
+ * 
+ * @author brucec5
+ *
+ */
+public class AudioMessenger extends Activity {
+	private static final String TAG = "ZenBox::AudioMessenger";
+	
+	private static AudioMessenger messenger = null;
+	
+	private PdService pdService;
+	
+	private final ServiceConnection connection;
+	
+	private AudioMessenger() {
+		connection = new ServiceConnection() {
+			@Override
+			public void onServiceConnected(ComponentName name, IBinder svc) {
+				pdService = ((PdService.PdBinder)svc).getService();
+				initPd();
+			}
+		
+			@Override
+			public void onServiceDisconnected(ComponentName name) {
+				// Never run or so I am told (required for interface)
+			}
+		};
+		
+		bindService(new Intent(this, PdService.class),
+				connection, BIND_AUTO_CREATE);
+	}
+	
+	/**
+	 * Return the instance of AudioMessenger
+	 * 
+	 * @return		the AudioMessenger instance 
+	 */
+	public static AudioMessenger getInstance() {
+		if (messenger == null) {
+			messenger = new AudioMessenger();
+		}
+		return messenger;
+	}
+
+	/**
+	 * Sends a bang to an object in the PD patch.
+	 * 
+	 * @param recv	symbol associated with the receiver
+	 * @return		error code, 0 on success
+	 */
+	public int sendBang(String recv) {
+		return PdBase.sendBang(recv);
+	}
+	
+	/**
+	 * Sends a float to an object in the PD patch.
+	 *  
+	 * @param recv	symbol associated with the receiver
+	 * @param x		the float to send
+	 * @return		error code, 0 on success
+	 */
+	public int sendFloat(String recv, float x) {
+		return PdBase.sendFloat(recv, x);
+	}
+	
+	/**
+	 * Sends a list to an object in the PD patch.
+	 * 
+	 * @param recv	symbol associated with the receiver
+	 * @param args	A list of arguments of type Integer, Float, or String
+	 * @return		error code, 0 on success
+	 */
+	public int sendList(String recv, Object... args) {
+		return PdBase.sendList(recv, args);
+	}
+	
+	/**
+	 * Sends a typed message to an object in the PD patch.
+	 * 
+	 * @param recv	symbol associated with the receiver
+	 * @param msg	first symbol of message
+	 * @param args	list of arguments to the message
+	 * 				of type Integer, Float, or String
+	 * @return		error code, 0 on success
+	 */
+	public int sendMessage(String recv, String msg, Object... args) {
+		return PdBase.sendMessage(recv, msg, args);
+	}
+	
+	private void initPd() {
+		Resources res = getResources();
+		File patch = null, audio = null;
+		
+		try {
+			PdBase.subscribe("android");
+			
+			InputStream inp = res.openRawResource(R.raw.synth);
+			InputStream ina = res.openRawResource(R.raw.icke);
+			
+			patch = IoUtils.extractResource(inp, "synth.pd", getCacheDir());
+			audio = IoUtils.extractResource(ina, "icke.wav", getCacheDir());
+			
+			PdBase.openPatch(patch);
+			
+			String name = res.getString(R.string.app_name);
+			
+			// -1 means use default, which should work for us.
+			pdService.initAudio(-1, -1, -1, -1);
+			pdService.startAudio(new Intent(this, ZenBoxActivity.class),
+					R.drawable.icon, name, name);
+		} catch (IOException e) {
+			Log.e(TAG, e.toString());
+		} finally {
+			if (patch != null)
+				patch.delete();
+			if (audio != null)
+				patch.delete();
+		}
+	}
+}
