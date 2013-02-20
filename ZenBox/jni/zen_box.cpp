263f94a0cc5e380df9767312747d5ccaed2c21fe
awdavies <a.w.davies.vio@gmail.com>
2013-02-19 19:52:24 -0800

    Added hpp file to clean things up.

--- ZenBox/jni/zen_box.cpp
+++ ZenBox/jni/zen_box.cpp
@@ -1,30 +1,17 @@
-#include <jni.h>
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/features2d/features2d.hpp>
-#include <vector>
-#include <iostream>
+#include "./zen_box.hpp"
 
-using std::vector;
-using namespace cv;
-
-extern "C" {
-
-JNIEXPORT void JNICALL Java_com_zenbox_ZenBoxActivity_OpticalFlow(JNIEnv*, jobject, jlong addrPrevMat, jlong addrCurMat, jlong addrPrevFeat, jlong addrCurFeat);
-
-JNIEXPORT void JNICALL Java_com_zenbox_ZenBoxActivity_OpticalFlow(JNIEnv*,
-		jobject, jlong addrPrevMat, jlong addrCurMat, jlong addrPrevFeat, jlong addrCurFeat) {
-	Mat& prevMat = *(Mat *) addrPrevMat;
-	Mat& curMat = *(Mat *) addrCurMat;
+JNIEXPORT void JNICALL Java_com_zenbox_ZenBoxActivity_OpticalFlow(
+		JNIEnv*, jobject, jlong addrPrevMat, jlong addrCurMat, jlong addrCurMatGray, jlong addrPrevFeat, jlong addrCurFeat) {
+	// Extract member variables from JVM.
+	Mat& prevMat = *(Mat *) addrPrevMat;			// Previous Matrix.
+	Mat& curMat = *(Mat *) addrCurMat;				// Current Matrix.
+	Mat& curMatGray = *(Mat *) addrCurMatGray;		// Current Matrix, Gray.
 	vector<KeyPoint> kpVec;
 
-	OrbFeatureDetector detector(35);
+	OrbFeatureDetector detector(MAX_FEATURES);
 	detector.detect(prevMat, kpVec);
 	for (unsigned int i = 0; i < kpVec.size(); ++i) {
 		const KeyPoint& kp = kpVec[i];
 		circle(curMat, kp.pt, 10, Scalar(255, 255, 255, 255));
 	}
 }
-}
-
-
---
+++ ZenBox/jni/zen_box.hpp
@@ -0,0 +1,68 @@
+/*
+ * zen_box.hpp
+ *
+ * This file contains a few auxiliary functions for extracting image
+ * data using native OpenCV code (to be used for basic Optical Flow
+ * detection initially.  This code will likely implement the majority of
+ * the grunt work for the image processing, returning only a few necessary
+ * values).
+ *
+ *      Author: Andrew Davies
+ */
+#include <jni.h>
+#include <stdint.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <vector>
+#include <iostream>
+
+using std::vector;
+using namespace cv;
+
+/* Constants. */
+const uint16_t MAX_FEATURES = 100;
+const Scalar FEATURE_COLOR = Scalar(255, 255, 255, 255);
+
+#ifndef ZEN_BOX_HPP_
+#define ZEN_BOX_HPP_
+
+/* Prototype Definitions. */
+#ifdef __cplusplus   // <--- for name mangling and such.
+extern "C" {
+#endif
+
+/**
+ * Detects the optical flow of the image, centered on the set of features
+ * detected between frames.  Requires the previous image, as well as the
+ * current image (and the features detected from both frames).  Note it is
+ * alright to call this function with some null values, as it will simply
+ * return not having done anything if any null values have been passed
+ * (excepting addrPrevMat, and addrPrevFeat).
+ *
+ * @param addrPrevMat The address of the matrix of the previous frame.
+ *
+ * @param addrCurMat The address of the matrix of the current frame.
+ *
+ * @param addrCurMatGray The address of the space to be used for converting
+ * 	the current image frame to grayscale.  This is to aid with robust feature
+ * 	detection.  Note this matrix need not be used outside of this function, and
+ * 	is simply necessary for space reasons (preventing allocation per every
+ * 	frame).
+ *
+ * @param addrPrevFeat The address of the previous image's features.  Note this will be
+ * 	modified by this function, and it is not intended for the caller to do any changes,
+ * 	and will only need to allocate the space for the object.
+ *
+ * @param addrCurFeat The address of the current image's features.  Note this will be
+ * 	modified by this function, and it is not intended for the caller to do any changes,
+ * 	and will only need to allocate the space for the object.
+ */
+JNIEXPORT void JNICALL Java_com_zenbox_ZenBoxActivity_OpticalFlow(
+		JNIEnv*, jobject, jlong addrPrevMat, jlong addrCurMat, jlong addrCurMatGray, jlong addrPrevFeat, jlong addrCurFeat);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEN_BOX_HPP_ */
